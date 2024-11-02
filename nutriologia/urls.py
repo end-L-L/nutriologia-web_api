2112f7b0a6966784c0be158ccc12e1d6e4405240
Boset <boss-x86@protonmail.com>
2024-11-02 01:17:44 -0600

    patch commit

--- nutriologia/urls.py
+++ nutriologia/urls.py
@@ -9,7 +9,7 @@ router = routers.DefaultRouter()
 urlpatterns = [
     #path("api/v1/", include(router.urls)),
     path("v1/nutriologos", nutriologo.NutriologosAll.as_view(), name="nutriologo-lista"),
-    path("v1/nutriologos/crear/", nutriologo.NutriologoView.as_view(), name="nutriologo-crear"),
+    path("v1/nutriologos/crear/", nutriologo.NutriologoViewPublic.as_view(), name="nutriologo-crear"),
     path("v1/nutriologos/obtener/", nutriologo.NutriologoView.as_view(), name="nutriologo-obtener"),
     path("v1/nutriologos/actualizar/", nutriologo.NutriologoView.as_view(), name="nutriologo-actualizar"),
     path("v1/nutriologos/eliminar/", nutriologo.NutriologoView.as_view(), name="nutriologo-eliminar"),
--- nutriologia/views/nutriologo.py
+++ nutriologia/views/nutriologo.py
@@ -14,17 +14,10 @@ from nutriologia.models import Nutriologo
 from nutriologia.serializers import UserSerializer
 from nutriologia.serializers import NutriologoSerializer
 
-class NutriologoView(APIView):
+class NutriologoViewPublic(APIView):
 
     authentication_classes = [] # no token
     #permission_classes = [AllowAny]
-    
-    # obtener nutriologo por ID - haciendo jwt
-    permission_classes = [IsAuthenticated]
-    def get(self, request, *args, **kwargs):
-        nutriologo = get_object_or_404(Nutriologo, id = request.GET.get("id"))
-        nutriologo = NutriologoSerializer(nutriologo, many=False).data
-        return Response(nutriologo, 200)
 
     # crear nutricionista
     #@authentication_classes([])
@@ -77,8 +70,28 @@ class NutriologoView(APIView):
         
             return Response({"nutritionist_created_id": nutriologo.id}, 201)
         return Response(user.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class NutriologosAll(APIView):
+    # obtener todos los nutriologos
+    permission_classes = [IsAuthenticated]
+    def get(self, request, *args, **kwargs):
+        nutriologos = Nutriologo.objects.all().order_by('id')
+        lista = NutriologoSerializer(nutriologos, many=True)
+        
+        return Response(lista.data, status=status.HTTP_200_OK)
+
+
+class NutriologoView(APIView):
+    # obtener nutriologo por ID - haciendo jwt
+    permission_classes = [IsAuthenticated]
+    def get(self, request, *args, **kwargs):
+        nutriologo = get_object_or_404(Nutriologo, id = request.GET.get("id"))
+        nutriologo = NutriologoSerializer(nutriologo, many=False).data
+        return Response(nutriologo, 200)
     
-    # actualizar nutriologo
+
+        # actualizar nutriologo
     permission_classes = [IsAuthenticated]
     def put(self, request, *args, **kwargs):
         nutriologo = get_object_or_404(Nutriologo, id = request.GET.get("id"))
@@ -110,13 +123,3 @@ class NutriologoView(APIView):
             return Response({"message": "nutriologo eliminado"}, 200)
         except:
             return Response({"message": "error al eliminar nutriologo"}, 400)
-
-
-class NutriologosAll(APIView):
-    # obtener todos los nutriologos
-    permission_classes = [IsAuthenticated]
-    def get(self, request, *args, **kwargs):
-        nutriologos = Nutriologo.objects.all().order_by('id')
-        lista = NutriologoSerializer(nutriologos, many=True)
-        
-        return Response(lista.data, status=status.HTTP_200_OK)

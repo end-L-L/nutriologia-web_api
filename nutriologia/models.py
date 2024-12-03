ace52c931d6d17c9cd442ca2245f9ccb97360d17
Boset <boss-x86@protonmail.com>
2024-12-03 14:45:59 -0600

    updated nutritionist

--- nutriologia/models.py
+++ nutriologia/models.py
@@ -4,14 +4,10 @@ from django.core.validators import RegexValidator
 
 class Nutriologo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    especialidad = models.CharField(max_length=100)
-    cedula = models.CharField(max_length=20, unique=True, validators=[
-        RegexValidator(regex=r'^\d{7,20}$', message="La cédula debe tener entre 7 y 20 dígitos.")
-    ])
-    telefono = models.CharField(max_length=15, validators=[
-        RegexValidator(regex=r'^\+?1?\d{9,15}$', message="El teléfono debe tener entre 9 y 15 dígitos.")
-    ])
-    direccion = models.CharField(max_length=255)
+    
+    cedula = models.CharField(max_length=20, unique=True)
+    telefono = models.CharField(max_length=15, unique=True)
+    
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
--- nutriologia/views/nutriologo.py
+++ nutriologia/views/nutriologo.py
@@ -7,77 +7,110 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 from nutriologia.models import Nutriologo
 from nutriologia.serializers import UserSerializer
 from nutriologia.serializers import NutriologoSerializer
 
+# public view sin autenticación
+class NutriologoViewPublic(APIView):
+    
+    # no auth
+    authentication_classes = [] # no token
+    permission_classes = [AllowAny]
+    
+    # crear nutricionista
+    def post(self, request, *args, **kwargs):
+    
+        user = UserSerializer(data=request.data)
+        try:        
+            if user.is_valid():
+                
+                first_name = request.data['first_name']
+                last_name = request.data['last_name']
+                email = request.data['email']
+                password = request.data['password']
+                #role = request.data['role']
+                username = request.data['username']
+                
+                # validar email
+                if User.objects.filter(email=email).exists():
+                    return Response({"message": "email ya registrado"}, 400)
+                
+                # validar username
+                if User.objects.filter(username=username).exists():
+                    return Response({"message": "username ya registrado"}, 400)
 
+                # validar cedula
+                cedula = request.data['cedula']
+                if len(cedula) < 8 or len(cedula) > 20:
+                    return Response({"message": "La cédula debe tener entre 8 y 20 dígitos."}, 400)
+                
+                cedula_paciente = Nutriologo.objects.filter(cedula=cedula)
+                if cedula_paciente.exists():
+                    return Response({"message": "Cédula ya registrada"}, 400)
+                
+                # validar telefono
+                telefono = request.data['telefono']
+                if len(telefono) < 8 or len(telefono) > 15:
+                    return Response({"message": "El teléfono debe tener entre 8 y 15 dígitos."}, 400)
+
+                telefono_paciente = Nutriologo.objects.filter(telefono=request.data['telefono'])
+                if telefono_paciente.exists():
+                    return Response({"message": "Teléfono ya registrado"}, 400)
+
+                # crear usuario
+                user = User.objects.create_user(
+                    username=username,
+                    first_name=first_name,
+                    last_name=last_name,
+                    email=email,
+                    is_active=1
+                )
+                
+                # password encriptada
+                user.set_password(password)
+
+                # rol de nutriologo
+                group, created = Group.objects.get_or_create(name='nutriologo')
+                group.user_set.add(user)
+
+                # crear perfil de nutriologo
+                nutriologo = Nutriologo.objects.create(user=user,
+                                                        cedula=request.data['cedula'],
+                                                        telefono=request.data['telefono']
+                                                        )
+                
+                nutriologo.save()
+
+                return Response({"nutritionist_created_id": nutriologo.id}, 201)
+            return Response(user.errors, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response({"message": "error al crear nutriologo"}, 400)
+
+class NutriologosAll(APIView):
+    # obtener todos los nutriologos
+    def get(self, request, *args, **kwargs):
+        nutriologos = Nutriologo.objects.all().order_by('id')
+        lista = NutriologoSerializer(nutriologos, many=True)
+        
+        return Response(lista.data, status=status.HTTP_200_OK)
+    
 class NutriologoView(APIView):
 
     # obtener nutriologo por ID - haciendo jwt
-    #permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     def get(self, request, *args, **kwargs):
         nutriologo = get_object_or_404(Nutriologo, id = request.GET.get("id"))
         nutriologo = NutriologoSerializer(nutriologo, many=False).data
         return Response(nutriologo, 200)
-
-    # crear nutricionista
-    def post(self, request, *args, **kwargs):
-        user = UserSerializer(data=request.data)
-        if user.is_valid():
-            first_name = request.data['first_name']
-            last_name = request.data['last_name']
-            email = request.data['email']
-            password = request.data['password']
-            #role = request.data['role']
-            username = request.data['username']
-            
-            # validar email
-            if User.objects.filter(email=email).exists():
-                return Response({"message": "email ya registrado"}, 400)
-            
-            # validar username
-            if User.objects.filter(username=username).exists():
-                return Response({"message": "username ya registrado"}, 400)
-
-            # crear usuario
-            user = User.objects.create_user(
-                username=username,
-                first_name=first_name,
-                last_name=last_name,
-                email=email,
-                is_active=1
-            )
-            
-            # password encriptada
-            user.set_password(password)
-            user.save()
-
-            # rol de nutriologo
-            group, created = Group.objects.get_or_create(name='Nutriologo')
-            group.user_set.add(user)
-            user.save()
-
-            # crear perfil de nutriologo
-            nutriologo = Nutriologo.objects.create(user=user,
-                                                       especialidad=request.data['especialidad'],
-                                                       cedula=request.data['cedula'],
-                                                       telefono=request.data['telefono'],
-                                                       direccion=request.data['direccion']
-                                                    )
-            nutriologo.save()
-
-            return Response({"nutritionist_created_id": nutriologo.id}, 201)
-        return Response(user.errors, status=status.HTTP_400_BAD_REQUEST)
     
     # actualizar nutriologo 
     def put(self, request, *args, **kwargs):
         nutriologo = get_object_or_404(Nutriologo, id = request.GET.get("id"))
-        nutriologo.especialidad = request.data['especialidad']
         nutriologo.cedula = request.data['cedula']
         nutriologo.telefono = request.data['telefono']
-        nutriologo.direccion = request.data['direccion']
         nutriologo.save()
 
         temp = nutriologo.user
@@ -101,12 +134,3 @@ class NutriologoView(APIView):
             return Response({"message": "nutriologo eliminado"}, 200)
         except:
             return Response({"message": "error al eliminar nutriologo"}, 400)
-
-
-class NutriologosAll(APIView):
-    # obtener todos los nutriologos
-    def get(self, request, *args, **kwargs):
-        nutriologos = Nutriologo.objects.all().order_by('id')
-        lista = NutriologoSerializer(nutriologos, many=True)
-        
-        return Response(lista.data, status=status.HTTP_200_OK)

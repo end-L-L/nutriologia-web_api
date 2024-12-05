2af81ff5693b335e2820ebe7f40698096ea28ee6
Boset <137344816+end-L-L@users.noreply.github.com>
2024-12-05 02:39:49 -0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -172,6 +172,7 @@ SIMPLE_JWT = {
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:4200',  # URL de Desarrollo de Angular
     'https://sprint-nutriologia.vercel.app',  # URL de Producción de Angular
+    'https://nutriologia.vercel.app'
 ]
 
 CORS_ALLOW_CREDENTIALS = True  # Cookies con CORS
